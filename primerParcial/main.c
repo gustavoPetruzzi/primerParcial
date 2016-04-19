fdc1a3ff20ec91929d854353e976e99391035096
mavocado3 <gustavopetruzzi19@gmail.com>
2016-04-19 01:26:51 -0300

    Added files via upload

--- primerParcial/main.c
+++ primerParcial/main.c
@@ -2,120 +2,164 @@
 #include <stdlib.h>
 #include "funciones.h"
 #include <string.h>
-#define MAX 10
+#define TAM 10
 typedef struct{
+    int dia,mes,anio;
 
+}eFecha;
+
+typedef struct{
     int legajo;
-    char nombre[50];
-    char apellido[50];
+    char nombre[30];
+    char apellido[30];
     float salario;
     char sector;
-    int isEmpty;
-}eEmpleados;
+}eEmpleado;
+eEmpleado cargarEmpleado(int, char[], char[], float, char);
+int buscarLibre(eEmpleado[], int);
+
 int main()
 {
-    int i;
-    int legajo[] = {1,4,21,8,5};
-    float sueldo[] = {1234.1, 15000.44, 13700.11, 8532, 7001,11};
-    char sector[] = {'1','2','3','4'};
-    char *nombre [] = {"Jose","Juan","Carlos","Roberto","Pepe"};
-    char *apellido[]= {"rodriguez", "Perez", "Alberto", "Carlos", "Mujica"};
+    int i,j;
     char opcion;
     int auxInt;
-    float auxFloat;
-    char auxChar;
-    char auxString[50];
     int respuesta;
-    eEmpleados empleado[50];
+    int legajo;
+    char nombre[30];
+    char apellido[30];
+    float salario;
+    char sector;
 
+    int auxlegajo[5] ={1,2,3,4,5};
+    char *auxnombre[5] ={"jose", "carlos", "juan","roberto", "puto"};
+    char *auxapellido[5]={"rodriguez","alberto","perugia","carlos","lee"};
+    float auxsalario[5]= {1234.13, 3214.12, 4456.62, 7773.12, 9932.33};
+    char auxsector[5]={'1', '2', '3', '1', '4'};
+    eEmpleado empleados[TAM];
 
-
-
-    for(i = 0; i<MAX; i++)
+    for(i = 0; i<TAM; i++)
     {
-        empleado[i].isEmpty = 1;
+        empleados[i].legajo = -1;
     }
 
     for(i = 0; i<5; i++)
     {
-        empleado[i].isEmpty = 0;
-        empleado[i].legajo = legajo[i];
-        empleado[i].salario = sueldo[i];
-        empleado[i].sector = sector[i];
-        strcpy( empleado[i].nombre, nombre[i]);
-        strcpy( empleado[i].apellido, apellido[i]);
+        empleados[i].legajo = auxlegajo[i];
+        empleados[i].salario = auxsalario[i];
+
+
     }
-    printf("%d\n", empleado[3].legajo);
-    opcion = mostrarMenu("1-ALTA\n2-MODIFICAR\n3-BAJAR\n4-INFORMAR\n5-LISTAR", '0', '5');
-    switch(opcion)
+
+
+    do
     {
-        case '1':
-            for(i = 0; i<MAX; i++)
-            {
-                if(empleado[i].isEmpty == 1)
+        opcion = mostrarMenu("1-ALTAS\n2-MODIFICAR\n3-BAJAS\n4-INFORMAR\n5-LISTAR\n6-SALIR", '1', '6');
+        switch(opcion)
+        {
+            case '1':
+                auxInt = buscarLibre(empleados, TAM);
+                printf("%d", auxInt);
+                if(auxInt !=-1)
                 {
-                    do
+
+                    respuesta = getName(nombre,"Ingrese el nombre:", "ERROR: solo se permiten letras", 2, 30);
+                    if(respuesta == -1)
                     {
-                        respuesta = getInt(&auxInt, "Ingrese su legajo:\n","ERROR: reingrese", 1, 10000);
-                        for(i = 0; i<MAX; i++)
-                        {
-                            if(auxInt == empleado[i].legajo)
-                            {
-                                printf("ESE LEGAJO YA EXISTE!");
-                                respuesta = -1;
-                                break;
-                            }
-                        }
-                    }while(respuesta!=0);
-                    printf("%d", auxInt);
-                    empleado[i].legajo = auxInt;
+                        respuesta = getName(nombre,"Ingrese el nombre:", "ERROR: solo se permiten letras", 2, 30);
+                    }
+
+                    respuesta = getName(apellido,"Ingrese el apellido:", "ERROR: solo se permiten letras", 2, 30);
+                    if(respuesta== -1)
+                    {
+                        respuesta = getName(apellido,"Ingrese el apellido:", "ERROR: solo se permiten letras", 2, 30);
+                    }
+
+                    respuesta = getFloat(&salario, "Ingrese el salario:", "ERROR: solo se permiten numeros", 2, 999999);
+                    if(respuesta == -1)
+                    {
+                        respuesta = getFloat(&salario, "Ingrese el salario:", "ERROR: solo se permiten numeros", 2, 999999);
+                    }
+
+                    respuesta = getChar(&sector,"Ingrese el sector\n1-Contabilidad\n2-Administracion\n3-Compras\n4-Ventas","ERROR", '1', '4' );
+                    if(respuesta == -1)
+                    {
+                        respuesta = getChar(&sector,"Ingrese el sector\n1-Contabilidad\n2-Administracion\n3-Compras\n4-Ventas","ERROR", '1', '4' );
+                    }
+
+                    if(auxInt == 0)
+                    {
+                        legajo = 1;
+
+                    }
+                    else
+                    {
+                        legajo = auxInt +1;
+                    }
 
 
-                    do
-                    {
-                        respuesta = getName(auxString, "Ingrese su nombre:\n","ERROR: reingrese", 2, 49);
-                    }while(respuesta!=0);
-                    strcpy(empleado[i].nombre, auxString);
 
-                    do
-                    {
-                        respuesta = getName(auxString, "Ingrese su Apellido:\n","ERROR: reingrese", 2, 49);
-                    }while(respuesta!=0);
-                    strcpy(empleado[i].apellido, auxString);
-                    do
-                    {
-                        respuesta = getFloat(&auxFloat, "Ingrese su salario:\n","ERROR: reingrese", 2, 999999);
-                    }while(respuesta!=0);
-                    empleado[i].salario = auxFloat;
 
-                    empleado[i].isEmpty = 0;
-                    /*printf("\tLegajo\tNombre\tApellido\tSalario\n");
-                    printf("\t%d\t%s\t%s\t%.2f\n", empleado[i].legajo, empleado[i].nombre, empleado[i].apellido, empleado[i].salario);*/
-                    break;
+                    empleados[auxInt] = cargarEmpleado(legajo, nombre, apellido, salario, sector);
+
                 }
-            }
-            break;
-        case '2':
-            break;
-        case '3':
-            break;
-        case '4':
-            break;
-        case '5':
-            break;
-
-    }
-
-    printf("\tLegajo\tNombre\tApellido\tSalario\n");
-    for(i = 0; i<MAX; i++)
+                else
+                {
+                    printf("Nomina de empleados llena!");
+                }
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
+                break;
+            case '6':
+                break;
+        }
+    }while(opcion!='6');
+    if(empleados[0].legajo != -1)
     {
-       /* if(empleado[i].isEmpty == 0)
-        {*/
-            printf("\t%d\t%s\t%s\t%.2f\n", empleado[i].legajo, empleado[i].nombre, empleado[i].apellido, empleado[i].salario);
-        //}
+        printf("Legajo\tNombre\tApellido\tSector\tSalario\n");
+        for(i=0;i<TAM; i++)
+        {
 
+            printf("%d\t%s\t%s", empleados[i].legajo, empleados[i].nombre, empleados[i].apellido);
+            printf("\t%c\t%.2f\n", empleados[i].sector, empleados[i].salario );
+        }
+    }
+    return 0;
+}
+
+eEmpleado cargarEmpleado(int legajo, char nombre[], char apellido[], float salario , char sector)
+{
+    eEmpleado auxEmpleado;
+    auxEmpleado.legajo = legajo;
+    strcpy(auxEmpleado.nombre, nombre);
+    strcpy(auxEmpleado.apellido, apellido);
+    auxEmpleado.salario = salario;
+    auxEmpleado.sector = sector;
+
+    return auxEmpleado;
+}
+
+
+
+int buscarLibre(eEmpleado empleado[], int MAX)
+{
+
+    int index = -1;
+    int i;
+
+    for(i= 0; i<MAX; i++)
+    {
+        if(empleado[i].legajo == -1)
+        {
+            index = i;
+            break;
+        }
     }
 
-    return 0;
-
+    return index;
 }
