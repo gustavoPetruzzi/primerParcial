cfa05df50597d7bd19bc5c1bb82ab925ca3cf250
mavocado3 <gustavopetruzzi19@gmail.com>
2016-04-20 01:30:57 -0300

    Update funciones.h

--- primerParcial/funciones.h
+++ primerParcial/funciones.h
@@ -3,4 +3,5 @@ int getFloat(float* input,char message[],char eMessage[], float lowLimit, float 
 int getChar(char* input,char message[],char eMessage[], char lowLimit, char hiLimit);
 int getString(char* input,char message[],char eMessage[], int lowLimit, int hiLimit);
 int getName(char* input,char message[],char eMessage[], int lowLimit, int hiLimit);
+char * validarNombre(char message[], char eMessage[], int lowLimit, int hiLimit);
 char mostrarMenu( char textomenu[], char min, char max );
